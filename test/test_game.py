ca07a2fe134afd173be2b8866f778507ab5c00af
Kevin Boulais <kboulais@gmail.com>
2024-11-19 12:06:33 +0100

    add game class to manage the battle

---
+++ test/test_game.py
@@ -0,0 +1,36 @@
+import unittest
+from src.person import Person
+from src.game import Game
+
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Person(with_armor=True)
+        self.player2 = Person()
+        self.game = Game(self.player1, self.player2)
+
+    def test_game_initialization(self):
+        self.assertEqual(self.game.player1, self.player1)
+        self.assertEqual(self.game.player2, self.player2)
+        self.assertEqual(self.game.round, 0)
+
+    def test_game_start(self):
+        winner = self.game.start(rounds=5)
+        self.assertIn(winner, [self.player1, self.player2])
+        self.assertTrue(self.game.round <= 5)
+
+    def test_game_winner(self):
+        self.player1.live = 50
+        self.player2.live = 0
+        winner = self.game.start(rounds=1)
+        self.assertEqual(winner, self.player1)
+
+    def test_game_draw(self):
+        self.player1.live = 0
+        self.player2.live = 0
+        winner = self.game.start(rounds=1)
+        self.assertIsNone(winner)
+
+
+if __name__ == '__main__':
+    unittest.main()
