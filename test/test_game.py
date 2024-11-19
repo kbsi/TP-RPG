4a1699aca09c360ac4f1d355ef3ceeb733739dba
Kevin Boulais <kboulais@gmail.com>
2024-11-19 15:31:14 +0100

    test coverage < 80%

--- test/test_game.py
+++ test/test_game.py
@@ -9,35 +9,6 @@ class TestGame(unittest.TestCase):
         self.player2 = Person()
         self.game = Game(self.player1, self.player2)
 
-    def test_game_initialization(self):
-        self.assertEqual(self.game.player1, self.player1)
-        self.assertEqual(self.game.player2, self.player2)
-        self.assertEqual(self.game.round, 1)
-
-    def test_game_start(self):
-        self.player1.live = 50
-        self.player2.live = 0
-        winner = self.game.start(rounds=1)
-        self.assertIn(winner, [self.player1, self.player2])
-        self.assertTrue(self.game.round <= 5)
-
-    def test_game_history(self):
-        self.game.start(rounds=1)
-        history = self.game.get_history()
-        self.assertTrue(len(history) > 0)
-
-    def test_game_winner(self):
-        self.player1.live = 50
-        self.player2.live = 0
-        winner = self.game.start(rounds=1)
-        self.assertEqual(winner, self.player1)
-
-    def test_game_draw(self):
-        self.player1.live = 0
-        self.player2.live = 0
-        winner = self.game.start(rounds=1)
-        self.assertIsNone(winner)
-
 
 if __name__ == '__main__':
     unittest.main()
